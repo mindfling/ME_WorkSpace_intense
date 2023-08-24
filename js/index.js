c9481ba7f08f2b12b0cddeb41f9c57baa9b29261
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2023-08-24 18:06:07 +0300

    add js script fetch

--- js/index.js
+++ js/index.js
@@ -1,8 +1,22 @@
-// поключаем скрипт для работы с селектом
+const API_URL = "https://workspace-methed.vercel.app/";
+const LOCATION_URL = "api/locations";
+
 console.log('hallo')
 
+// поключаем скрипт для работы с селектом
 const citySelect = document.querySelector('#city');
 const cityChoices = new Choices(citySelect, {
-  searchEnabled: false,
+  searchEnabled: true,
   itemSelectText: "",
 });
+
+
+// * получение данных по API
+fetch(API_URL + LOCATION_URL)
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => console.log(data))
+  .catch((error) => console.log(error));
+  
+  
